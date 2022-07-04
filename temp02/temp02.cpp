b4d318179ba468f592875cb6a37f07542979fcfe
maievskyi <maevua2@gmail.com>
2022-07-04 11:05:21 +0300

    start 00

--- temp02/temp02.cpp
+++ temp02/temp02.cpp
@@ -1,11 +1,14 @@
 // temp02.cpp: определяет точку входа для консольного приложения.
-//
+// start 00
 
 #include "stdafx.h"
-
+#include <stdlib.h>
 
 int main()
 {
+
+
+	system("pause");
     return 0;
 }
 
